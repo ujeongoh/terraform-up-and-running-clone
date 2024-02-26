a7527df2eb5526da225ebba22c5ccc89bc9415c5
ujeongoh <yujoh94@gmail.com>
2024-02-26 21:25:32 +0900

    add port variable

--- web-server/variables.tf
+++ web-server/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   default = "ap-northeast-2"
 }
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+#  default     = 8080
+}
